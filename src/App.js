b9df2a107970c45d534f9c3980d670ae28c25360
Suresh Korlapati <sureshkorlapati@gmail.com>
2018-12-13 06:31:31 -0600

    changed app title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Checkout App - shop at your local stores.
+            Checkout App - best way to shop at your local stores.
           </p>
           
         </header>
